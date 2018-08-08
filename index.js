a1b8652f82a3333f2994b2cc61443da68b6438c3
yossivcita <yossi@vcita.com>
2018-08-08 11:44:02 +0300

    test

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ var vm = new Vue({
   }
 })
 
+$.ajaxPrefilter( function (options) {
+  if (options.crossDomain && jQuery.support.cors) {
+    var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+    //options.url = "http://cors.corsproxy.io/url=" + options.url;
+  }
+});
+
 $( "#myframe" ).load( "https://www.meet2know.com/login?sso=true", function() {
   alert( "Load was performed." );
 });
